40e87ca1041ad69e4d935957332128a1d1102fab
Shabari_K_S <95925800+Shabari-K-S@users.noreply.github.com>
2021-12-14 19:54:22 +0530

    Update encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -6,7 +6,7 @@ message = input("Enter the message to be encrypted:")
 
 alphabet = 'QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm!@#$%^&*()_\{\}+-=|[] 1234567890'
 
-key = int(input("Enter the key:"))   		# key the value to change the message for alphabets
+key = int(input("Enter the Password to encrypt your message:"))   		# key the value to change the message for alphabets
 
 encrypt = ''		# encrypt is the variable to store the data after change
 
